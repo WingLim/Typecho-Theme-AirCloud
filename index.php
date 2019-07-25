51ec2c309120a999020f19dd1be0511f7a8491e9
WingLim <643089849@qq.com>
2019-07-26 00:35:36 +0800

    精简缩进

--- index.php
+++ index.php
@@ -10,27 +10,25 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  $this->need('header.php');
 ?>
-    <div class="index-middle">
-        <div class="post-preview-container" style="min-height: <%- config.per_page * 72 %>px">
-            <?php while($this->next()): ?>
-            <div class="post-preview">
-                <div class="post-time"><?php $this->date(); ?></div>
-                <div class="post-info">
-                    <a href="<?php $this->permalink(); ?>">
-                        <h3>
-                        <?php $this->title() ?>
-                        </h3>
-                    </a>
-                    <p class="tag">
-                        <span>/</span>
-                        <?php $this->tags('', true, 'none'); ?>
-                    </p>
-                </div>
+<div class="index-middle">
+    <div class="post-preview-container" style="min-height: <%- config.per_page * 72 %>px">
+        <?php while($this->next()): ?>
+        <div class="post-preview">
+            <div class="post-time"><?php $this->date(); ?></div>
+            <div class="post-info">
+                <a href="<?php $this->permalink(); ?>">
+                    <h3><?php $this->title() ?></h3>
+                </a>
+                <p class="tag">
+                    <span>/</span>
+                    <?php $this->tags('', true, 'none'); ?>
+                </p>
             </div>
-            <?php endwhile; ?>
         </div>
-        <ul class="pager">
-            <?php $this->pageNav('&larr; Newer Posts', 'Older Posts &rarr;'); ?>
-        </ul>
+        <?php endwhile; ?>
     </div>
+    <ul class="pager">
+        <?php $this->pageNav('&larr; Newer Posts', 'Older Posts &rarr;'); ?>
+    </ul>
+</div>
 <?php $this->need('footer.php'); ?>
--- single.php
+++ single.php
@@ -2,31 +2,31 @@
 $this->need('header.php');
 ?>
 
-    <div class="index-middle">
-        <div class="post-container">
-            <div class="post-title">
-                <?php $this->title() ?>
+<div class="index-middle">
+    <div class="post-container">
+        <div class="post-title">
+            <?php $this->title() ?>
+        </div>
+        <?php if($this->is('post')): ?>
+        <div class="post-meta">
+            <span class="attr">发布于：<span><?php $this->date() ?></span></span>
+            <span class="attr tag">标签：/
+                <?php $this->tags('', true, 'none'); ?>
+            </span>
+            <span class="attr">访问：<span id="busuanzi_value_page_pv"></span></span>
+        </div>
+        <?php endif;?>
+        <div class="post-content<?php if($this->options->indent == 'disable'): ?> no-indent<?php endif; ?>">
+            <?php $this->content(); ?>
+            <br />
+            <div id="comment-container">
             </div>
-            <?php if($this->is('post')): ?>
-            <div class="post-meta">
-                <span class="attr">发布于：<span><?php $this->date() ?></span></span>
-                <span class="attr tag">标签：/
-                    <?php $this->tags('', true, 'none'); ?>
-                </span>
-                <span class="attr">访问：<span id="busuanzi_value_page_pv"></span></span>
-            </div>
-            <?php endif;?>
-            <div class="post-content<?php if($this->options->indent == 'disable'): ?> no-indent<?php endif; ?>">
-                <?php $this->content(); ?>
-                <br />
-                <div id="comment-container">
-                </div>
-                <div id="disqus_thread"></div>
+            <div id="disqus_thread"></div>
 
-                <div id="lv-container">
-                </div>
-
+            <div id="lv-container">
             </div>
+
         </div>
     </div>
+</div>
 <?php $this->need('footer.php'); ?>
