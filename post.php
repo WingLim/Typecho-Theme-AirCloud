72868b4fed0d5910de90b6ff53466ac0086d2329
WingLim <643089849@qq.com>
2019-07-26 00:14:46 +0800

    1.0

--- post.php
+++ post.php
@@ -1,39 +1,31 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; 
+$this->need('header.php');
+?>
 
+    <div class="index-middle">
+        <div class="post-container">
+            <div class="post-title">
+                <?php $this->title() ?>
+            </div>
+            <div class="post-meta">
+                <span class="attr">发布于：<span><?php $this->date() ?></span></span>
+                <span class="attr tag">标签：/
+                    <?php $this->tags('', true, 'none'); ?>
+                </span>
+                <span class="attr">访问：<span id="busuanzi_value_page_pv"></span></span>
+            </div>
+            <div class="post-content <% if(config.post_style && !config.post_style.indent) {%>no-indent<% } %>">
+                <?php $this->content(); ?>
+                <br />
+                <div id="comment-container">
+                </div>
+                <div id="disqus_thread"></div>
 
+                <div id="lv-container">
+                </div>
 
-<div class="post-container">
-    <div class="post-title">
-        <%- page.title.replace(/[<>&"]/g,function(c){
-            return {'<':'&lt;','>':'&gt;','&':'&amp;','"':'&quot;'}[c];
-        }) %>
-    </div>
-
-    <div class="post-meta">
-        <span class="attr"><%= __('post.postTime') %>：<span><%- page.date.format(config.date_format) +" "+ page.date.format(config.time_format)%></span></span>
-        <% if (page.tags.length){ %>
-        <span class="attr"><%= __('post.tags') %>：/
-        <% page.tags.forEach(function(tag){ %>
-        <a class="tag" href="<%= config.root %>tags/#<%= tag.name %>" title="<%= tag.name %>"><%= tag.name %></a>
-        <span>/</span>
-        <% }) %>
-        <% } %>
-        </span>
-        <span class="attr"><%= __('post.visit') %>：<span id="busuanzi_value_page_pv"></span>
-</span>
-</span>
-    </div>
-    <div class="post-content <% if(config.post_style && !config.post_style.indent) {%>no-indent<% } %>">
-        <%- page.content %>
-        <% if(config.donate){%>
-            <%- partial('_partial/donate', null, {cache: false}) %>
-        <% } %>
-        <br />
-        <div id="comment-container">
+            </div>
         </div>
-        <div id="disqus_thread"></div>
-
-        <div id="lv-container">
-        </div>
-
     </div>
 </div>
+<?php $this->need('footer.php'); ?>
