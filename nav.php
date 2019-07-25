a0392100abcb35bdf465fdf35ec9ef5d867be8b3
WingLim <643089849@qq.com>
2019-07-25 23:12:11 +0800

    copy from nav.ejs

---
+++ nav.php
@@ -0,0 +1,67 @@
+
+<div class="nav" id="nav">
+    <div class="avatar-name">
+        <div class="avatar <% if(config.avatar_style && config.avatar_style.radius) {%>radius<% } %>">
+            <img src="<%= config['root'] %><%= config['sidebar-avatar'] %>" />
+        </div>
+        <div class="name">
+            <i><%= config.author %></i>
+        </div>
+    </div>
+    <div class="contents" id="nav-content">
+        <ul>
+            <li <% if(is_home()){ %> <%- "class=active" %> <% } %>>
+                <a href="<%= url_for(config.home_dir || '/') %>">
+                    <i class="iconfont icon-shouye1"></i>
+                    <span><%= __('nav.home') %></span>
+                </a>
+            </li>
+            <li <% if(is_current('tags')){ %> <%- "class=active" %> <% } %>>
+                <a href="<%= url_for(config.tag_dir) %>">
+                    <i class="iconfont icon-biaoqian1"></i>
+                    <span><%= __('nav.tags') %></span>
+                </a>
+            </li>
+            <li <% if(is_current('archive')){ %> <%- "class=active" %> <% } %>>
+                <a href="<%= url_for(config.archive_dir) %>">
+                    <i class="iconfont icon-guidang2"></i>
+                    <span><%= __('nav.archives') %></span>
+                </a>
+            </li>
+            <li <% if(is_current('about')){ %> <%- "class=active" %> <% } %>>
+                <a href="<%= config.root %><%- 'about/' %>">
+                    <i class="iconfont icon-guanyu2"></i>
+                    <span><%= __('nav.about') %></span>
+                </a>
+            </li>
+            <%if(config.search && config.search.path){%>
+            <li>
+                <a id="search">
+                    <i class="iconfont icon-sousuo1"></i>
+                    <span><%= __('nav.search') %></span>
+                </a>
+            </li>
+            <% } %>
+        </ul>
+    </div>
+    <%
+        if(is_post()){
+        // if(/[0-9]*\/[0-9]*\/[0-9]*\//.test(path)){
+    %>
+        <%- partial('./toc', {item: page, index: false}) %>
+    <% } %>
+</div>
+
+
+<div class="search-field" id="search-field">
+    <div class="search-container">
+        <div class="search-input">
+            <span id="esc-search"> <i class="icon-fanhui iconfont"></i></span>
+            <input id="search-input"/>
+            <span id="begin-search"><%= __('search.search') %></span>
+        </div>
+        <div class="search-result-container" id="search-result-container">
+
+        </div>
+    </div>
+</div>
