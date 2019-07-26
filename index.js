dc66000233c1797a1e53b62b309a19067f20a2ba
WingLim <643089849@qq.com>
2019-07-26 11:20:18 +0800

    页面调整时自动恢复菜单

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Global functions and listeners
+window.onresize = () => {
+    // when window resize , we show remove some class that me be added
+    // often for debug
+    if(window.document.documentElement.clientWidth > 680){
+        let aboutContent = document.getElementById('nav-content')
+        aboutContent.classList.remove('hide-block')
+        aboutContent.classList.remove('show-block');
+    }
+};
 // Nav switch function on mobile
 const navToggle = document.getElementById('site-nav-toggle');
 navToggle.addEventListener('click', () => {
