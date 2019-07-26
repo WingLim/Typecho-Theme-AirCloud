edcddaa41f5d2501c82593b06d82151842f5544d
WingLim <643089849@qq.com>
2019-07-26 21:03:52 +0800

    修复bug

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
         </div>
         </div>
+        <?php if($this->is('index')): ?>
         <div class="pager" id="load_more">
             <?php $this->pageLink('加载更多文章','next'); ?>
         </div>
+        <?php endif; ?>
         <footer class="footer">
         <p>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a></p>
             <p>Power By <a href="https://typecho.org/" target="_blank">Typecho</a>  Theme <a href="https://github.com/aircloud/hexo-theme-aircloud">AirCloud</a></p>
