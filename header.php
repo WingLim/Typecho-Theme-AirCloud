9e7e2f846d2d9cc78bd758a0f6494f868f119f40
WingLim <643089849@qq.com>
2019-08-03 00:40:01 +0800

    :speech_balloon:change en to zh-cn

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-cn">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
